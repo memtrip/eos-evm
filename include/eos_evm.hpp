44ea39468468584331dd62a8f2db92f731a7ee8d
Samuel Kirton <samkirton@me.com>
2020-02-14 17:19:52 +0100

    => Removed the unnecessary evm namespace
    => Added the Account multi index table

--- include/eos_evm.hpp
+++ include/eos_evm.hpp
@@ -1,23 +1,31 @@
+#include <eosio/asset.hpp>
 #include <eosio/eosio.hpp>
 
 using namespace std;
 using namespace eosio;
 
-namespace evm {
-  CONTRACT eos_evm : public contract {
-    public:
-      using contract::contract;
+CONTRACT eos_evm : public contract {
+  public:
+    using contract::contract;
 
-      ACTION writelog(name from, string message);
-      ACTION clearlog();
+    ACTION writelog(name from, string message);
+    ACTION clearlog();
 
-    private:
-      TABLE log {
-        uint64_t key;
-        name    user;
-        string  message;
-        uint64_t primary_key() const { return key; }
-      };
-      typedef multi_index<name("log"), log> log_table;
-  };
-}
+  private:
+    TABLE account {
+      name account;
+      uint64_t nonce;
+      asset balance;
+
+      name primary_key() const { return account; }
+    };
+    typedef multi_index<name("account"), account> account_table;
+
+    TABLE log {
+      uint64_t key;
+      name    user;
+      string  message;
+      uint64_t primary_key() const { return key; }
+    };
+    typedef multi_index<name("log"), log> log_table;
+};
--- src/eos_evm.cpp
+++ src/eos_evm.cpp
@@ -1,28 +1,26 @@
 #include <eos_evm.hpp>
 
-namespace evm {
-  ACTION evm::eos_evm::writelog(name from, string message) {
-    require_auth(get_self());
-    
-    log_table _log(get_self(), get_self().value);
+ACTION eos_evm::writelog(name from, string message) {
+  require_auth(get_self());
+  
+  log_table _log(get_self(), get_self().value);
 
-    _log.emplace(from, [&](auto& log) {
-      log.key = _log.available_primary_key();
-      log.user = from;
-      log.message = message;
-    });
-  }
+  _log.emplace(from, [&](auto& log) {
+    log.key = _log.available_primary_key();
+    log.user = from;
+    log.message = message;
+  });
+}
 
-  ACTION evm::eos_evm::clearlog() {
-    require_auth(get_self());
+ACTION eos_evm::clearlog() {
+  require_auth(get_self());
 
-    log_table _log(get_self(), get_self().value);
+  log_table _log(get_self(), get_self().value);
 
-    auto log_itr = _log.begin();
-    while (log_itr != _log.end()) {
-      log_itr = _log.erase(log_itr);
-    }
+  auto log_itr = _log.begin();
+  while (log_itr != _log.end()) {
+    log_itr = _log.erase(log_itr);
   }
 }
 
-EOSIO_DISPATCH(evm::eos_evm, (writelog)(clearlog))
+EOSIO_DISPATCH(eos_evm, (writelog)(clearlog))
