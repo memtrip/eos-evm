bde788c5e6b38403bba04be349eb36f15d47afc6
Samuel Kirton <samkirton@me.com>
2020-03-03 22:14:55 +0100

    => Added the gas constants

--- src/evm/include/evm/gasometer.h
+++ src/evm/include/evm/gasometer.h
@@ -1,9 +1,36 @@
+#pragma once
 #include <variant>
 #include <evm/opcode.h>
 #include <evm/instruction.h>
 #include <evm/external.h>
 #include <evm/stack.h>
 
+constexpr uint8_t TIER_STEP_GAS[] = { 0, 2, 3, 5, 8, 10, 20, 0 };
+
+const size_t CALL_STIPEND = 2300;
+const size_t SSTORE_SET_GAS = 20000;
+const size_t SSTORE_RESET_GAS = 5000;
+const size_t SLOAD_GAS = 200;
+const size_t BALANCE_GAS = 400;
+const size_t EXTCODESIZE_GAS = 20;
+const size_t EXTCODEHASH_GAS = 400;
+const bool NO_EMPTY = false;
+const size_t SHA3_GAS = 30;
+const size_t SHA3_WORD_GAS = 6;
+const size_t EXTCODECOPY_BASE_GAS = 20;
+const size_t LOG_GAS = 375;
+const size_t LOG_TOPIC_GAS = 375;
+const size_t LOG_DATA_GAS = 8;
+const size_t CALL_GAS = 40;
+const size_t CALL_NEW_ACCOUNT_GAS = 25000;
+const size_t CALL_VALUE_TRANSFER_GAS = 9000;
+const size_t CREATE_GAS = 32000;
+const size_t EXP_GAS = 10;
+const size_t EXP_BYTE_GAS = 10;
+const size_t BLOCK_HASH_GAS = 20;
+const size_t COPY_GAS = 3;
+const size_t MEMORY_GAS = 3;
+const size_t QUAD_COEFF_DIV = 512;
 
 struct InstructionRequirements {
   size_t gasCost;
