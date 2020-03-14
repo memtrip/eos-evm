7d32e95265fa8ecd6c81076679126be50fb3f8be
Samuel Kirton <samkirton@me.com>
2020-03-14 20:17:17 +0100

    => Added a test for the most basic form of contract

--- evm_tests/vm.test_create_contract.test.cpp
+++ evm_tests/vm.test_create_contract.test.cpp
@@ -7,6 +7,40 @@
 #include <evm/gasometer.h>
 #include "external_mock.h"
 
+TEST_CASE("Create the most basic contract", "[create]") {
+  std::string bytecode_str = "6080604052348015600f57600080fd5b50603580601d6000396000f300";
+  params_t params =  Utils::createParams(Hex::hexToBytes(bytecode_str), bytes_t());
+  ExternalMock ext {};
+  VM vm {};
+  ReturnData returnData = ReturnData::empty();
+Call call(0);
+  account_store_t* accountItems = new account_store_t();
+  AccountState as(accountItems);
+  Gasometer gasometer(0);
+  bytes_t* memoryBytes = new bytes_t();
+  Memory mem(memoryBytes);
+  std::vector<uint256_t>* stackItems = new std::vector<uint256_t>();
+  StackMachine sm(stackItems);
+
+  // when
+  exec_result_t vm_result = vm.execute(mem, sm, as, gasometer, params, ext,  call, Utils::env());
+
+  // then
+  REQUIRE(ExecResult::DONE == vm_result.first);
+
+  // and then
+  gas_left_t gasLeft = std::get<gas_left_t>(vm_result.second);
+
+  REQUIRE(GasType::NEEDS_RETURN == gasLeft.first);
+
+  // and then
+  NeedsReturn needsReturn = std::get<NeedsReturn>(gasLeft.second);
+
+  CHECK("" == 
+    Hex::bytesToHex(needsReturn.data.mem)
+  );
+}
+
 TEST_CASE("Create contract using CODECOPY", "[create]") {
   std::string bytecode_str = "608060405234801561001057600080fd5b507fb8a00d6d8ca1be30bfec34d8f97e55f0f0fd9eeb7fb46e030516363d4cfe1ad630604051808273ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff16815260200191505060405180910390a160b8806100826000396000f3fe6080604052348015600f57600080fd5b506004361060285760003560e01c8063771602f714602d575b600080fd5b606060048036036040811015604157600080fd5b8101908080359060200190929190803590602001909291905050506076565b6040518082815260200191505060405180910390f35b600081830190509291505056fea265627a7a723158209551755b4e59ca6cf78d79f5356d91565950805937074458700610f23c6ecf9b64736f6c63430005100032";
   params_t params =  Utils::createParams(Hex::hexToBytes(bytecode_str), bytes_t());
